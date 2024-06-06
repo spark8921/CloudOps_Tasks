132cfeaa336f2b5b155c2eded41ab0339a8397fd
Spandana_Techie <169144373+spark8921@users.noreply.github.com>
2024-06-06 18:52:04 +0530

    Delete IAM directory

--- IAM/main.tf
+++
@@ -1,5 +0,0 @@
-module "iam_user" {
- source = "./modules/iam_user"
- aws_region = var.aws_region
- iam_user_name = var.iam_user_name
-}
--- IAM/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_region = "ap-southeast-1"
-iam_user_name = "sample_user"
